b5315682929d2a6b4af13aabbe2a6e2840bba359
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-08-02 16:24:59 +0200

    Complete exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,59 @@
 import { useState } from "react";
 
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const RenderButtons = ({ onGoodClick, onNeutralClick, onBadClick }) => {
+  return (
+    <div>
+      <Button text="good" handleClick={onGoodClick}></Button>
+      <Button text="neutral" handleClick={onNeutralClick}></Button>
+      <Button text="bad" handleClick={onBadClick}></Button>
+    </div>
+  );
+};
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const RenderStatistics = ({
+  goodStatistics,
+  neutralStatistics,
+  badStatistics,
+}) => {
+  return (
+    <div>
+      <p>good {goodStatistics}</p>
+      <p>neutral {neutralStatistics}</p>
+      <p>bad {badStatistics}</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <Header text="give feedback"></Header>
+      <RenderButtons
+        onGoodClick={() => setGood(good + 1)}
+        onNeutralClick={() => setNeutral(neutral + 1)}
+        onBadClick={() => setBad(bad + 1)}
+      ></RenderButtons>
+      <Header text="statistics"></Header>
+      <RenderStatistics
+        goodStatistics={good}
+        neutralStatistics={neutral}
+        badStatistics={bad}
+      ></RenderStatistics>
+    </div>
+  );
 };
 
 export default App;
