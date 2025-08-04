a276be6d26f3f274989fa49d351b42e3163f7a0d
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-08-04 16:58:05 +0200

    Declare Course component as a separate module (complete exercise 2.5)

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,37 @@
+const Header = ({ props }) => <h1>{props.name}</h1>;
+
+const Content = ({ props }) => {
+  return (
+    <div>
+      {props.map((part) => (
+        <Part key={part.id} props={part}></Part>
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ props }) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+const Total = ({ props }) => {
+  const totalExercises = props.reduce((sum, prop) => sum + prop.exercises, 0);
+
+  return <p>Total of {totalExercises} exercises</p>;
+};
+
+const Course = ({ courses }) => {
+  return (
+    <>
+      <Header props={courses}></Header>
+      <Content props={courses.parts}></Content>
+      <Total props={courses.parts}></Total>
+    </>
+  );
+};
+
+export default Course;
