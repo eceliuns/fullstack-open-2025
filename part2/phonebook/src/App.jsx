dfbe7a44de5bb5904a887778d299c8d3a927d41e
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-08-16 17:19:35 +0200

    Save new phonebook entries to backend using axios POST (complete exercise 2.12)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,12 +5,7 @@ import PersonList from "./components/PersonList";
 import PersonForm from "./components/PersonForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    // { name: "Arto Hellas", number: "040-123456", id: 1 },
-    // { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    // { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    // { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
@@ -36,9 +31,14 @@ const App = () => {
       return;
     }
 
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+    axios
+      .post("http://localhost:3001/persons", personObject)
+      .then((response) => {
+        console.log("this is addPerson response:", response);
+        setPersons(persons.concat(response.data));
+        setNewName("");
+        setNewNumber("");
+      });
   };
 
   const handleNameChange = (event) => {
