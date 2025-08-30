98e8ba9ba907384e09b4c5b03bb0df5d0f85a120
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-08-30 16:11:55 +0200

    Create button to delete people from the phonebook (complete exercise 2.14)

--- part2/phonebook/src/components/PersonList.jsx
+++ part2/phonebook/src/components/PersonList.jsx
@@ -1,12 +1,16 @@
 import Person from "./Person";
 
-const PersonList = ({ persons, newFilter }) => {
+const PersonList = ({ persons, newFilter, deletePerson }) => {
   let filteredPeople = persons.filter((person) =>
     person.name.toLowerCase().includes(newFilter.toLowerCase())
   );
 
   return filteredPeople.map((person) => (
-    <Person key={person.id} person={person}></Person>
+    <Person
+      key={person.id}
+      person={person}
+      deletePerson={() => deletePerson(person.id)}
+    ></Person>
   ));
 };
 
