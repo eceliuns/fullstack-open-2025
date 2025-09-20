fa64988b25b80206fe0d1bc4b51945311c975727
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-09-20 15:14:33 +0200

    Expand the backend so that new phonebook entries can be added by making HTTP POST requests to the address http://localhost:3001/api/persons (complete exercise 3.5)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -44,6 +46,34 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+const generateId = () => {
+  let id = Math.floor(Math.random() * 1_000_000_000).toString();
+  while (persons.some((person) => person.id === id)) {
+    id = Math.floor(Math.random() * 1_000_000_000).toString();
+  }
+
+  return id;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 app.get("/info", (request, response) => {
