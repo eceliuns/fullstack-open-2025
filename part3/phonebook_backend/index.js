36bc897301d27b0656a56f77c845f47244238238
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-09-20 17:28:43 +0200

    Make phonebook backend work with phonebook frontend from part 2 (complete exercise 3.9)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
 
 const morgan = require("morgan");
 app.use(
