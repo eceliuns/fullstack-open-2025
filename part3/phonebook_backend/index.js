8c32e641daaa37781d7f10ce15b3fa5b475e9b9d
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-09-20 15:28:25 +0200

    Implement error handling for creating new entries when name or number is missing or when name is a duplicate (complete exercise 3.6)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -59,10 +59,20 @@ const generateId = () => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (!body.name && !body.number) {
+    return response.status(400).json({ error: "name and number missing" });
+  }
+
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({ error: "name must be unique" });
   }
 
   const person = {
