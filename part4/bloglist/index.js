d7b4b32186c28362c86139903887afa18f3f0cbb
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-11-01 15:20:39 +0100

    Refactor application in separate moduls and add error handling (complete exercise 4.2)

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,36 +1,55 @@
-const express = require("express");
-const mongoose = require("mongoose");
 require("dotenv").config();
+const express = require("express");
+const Blog = require("./models/blog");
 
 const app = express();
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+app.get("/api/blogs", (request, response, next) => {
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/blogs", (request, response) => {
+app.post("/api/blogs", (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  if (error.name === "NotFound") {
+    return response.status(404).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
