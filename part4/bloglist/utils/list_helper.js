51b5809824e83a705c0fa057eb712f29ad38315c
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-11-09 18:29:42 +0100

    Define and test mostBlogs and mostLikes functions

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -18,8 +18,58 @@ const favoriteBlog = (blogs) => {
   return favorite.title;
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authors = {};
+  const mostBlog = {
+    author: "",
+    blogs: 0,
+  };
+
+  blogs.forEach((blog) => {
+    authors[blog.author] = authors[blog.author] ? authors[blog.author] + 1 : 1;
+  });
+
+  for (const [author, blogs] of Object.entries(authors)) {
+    if (blogs > mostBlog.blogs) {
+      mostBlog.author = author;
+      mostBlog.blogs = blogs;
+    }
+  }
+  return mostBlog;
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authors = {};
+  const mostLikes = {
+    author: "",
+    likes: 0,
+  };
+
+  blogs.forEach((blog) => {
+    authors[blog.author] = (authors[blog.author] || 0) + blog.likes;
+  });
+
+  for (const [author, likes] of Object.entries(authors)) {
+    if (likes > mostLikes.likes) {
+      mostLikes.author = author;
+      mostLikes.likes = likes;
+    }
+  }
+  return mostLikes;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
